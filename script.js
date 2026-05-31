22696a72e1a72307989ee1f7c168f4a4178e5793
Eduardo Martins <eduardoimartinsdasilva50@gmail.com>
2026-05-31 16:48:29 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,47 +1,26 @@
-// Executa quando toda a página terminar de carregar
 $(document).ready(function () {
 
-    // Seleciona o envelope
-    var envelope = $('#envelope');
+    const envelope = $('#envelope');
+    const letter = $('.letter');
 
-    // Seleciona o botão Open
-    var btn_open = $("#open");
+    // Primeiro clique: abre envelope
+    envelope.on('click', function () {
 
-    // Seleciona o botão Reset
-    var btn_reset = $('#reset');
-
-    // Ao clicar no envelope
-    envelope.click(function () {
-        open();
+        if (envelope.hasClass('close')) {
+            envelope.removeClass('close')
+                    .addClass('open');
+        }
     });
 
-    // Ao clicar no botão Open
-    btn_open.click(function () {
-        open();
+    // Segundo clique: zoom na carta
+    letter.on('click', function (e) {
+
+        // impede que o clique suba para o envelope
+        e.stopPropagation();
+
+        if (envelope.hasClass('open')) {
+            envelope.addClass('zoom');
+        }
     });
 
-    // Ao clicar no botão Reset
-    btn_reset.click(function () {
-        close();
-    });
-
-    // Função que abre o envelope
-    function open() {
-
-        // Adiciona a classe open
-        envelope.addClass('open')
-
-            // Remove a classe close
-            .removeClass('close');
-    }
-
-    // Função que fecha o envelope
-    function close() {
-
-        // Adiciona a classe close
-        envelope.addClass('close')
-
-            // Remove a classe open
-            .removeClass('open');
-    }
 });
